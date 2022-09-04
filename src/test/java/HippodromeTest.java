5519c25b4719a7b628caccdada4d9d9f447a54dd
oovcharenko <sw77@mail.ru>
2022-09-04 22:50:00 +0300

    First

---
+++ src/test/java/HippodromeTest.java
@@ -0,0 +1,21 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HippodromeTest {
+
+    @ParameterizedTest
+    @CsvSource
+    void getHorses() {
+    }
+
+    @Test
+    void move() {
+    }
+
+    @Test
+    void getWinner() {
+    }
+}
