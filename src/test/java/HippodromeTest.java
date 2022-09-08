a0405fa70e2b347e1c7b4b7a4bb987c7fc85d679
oovcharenko <sw77@mail.ru>
2022-09-08 22:22:01 +0300

    First

--- src/test/java/HippodromeTest.java
+++ src/test/java/HippodromeTest.java
@@ -1,18 +1,29 @@
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.provider.NullSource;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@ExtendWith(MockitoExtension.class)
 class HippodromeTest {
 
-    @ParameterizedTest
-    @CsvSource
+    @Test
+    @NullSource
+    void hippodromeWithNullParameter(List<Horse> horses) {
+        Throwable exception = assertThrows(IllegalArgumentException.class, () -> new Hippodrome(horses));
+        assertEquals("Horses cannot be null.", exception.getMessage());
+    }
+
+    @Test
     void getHorses() {
     }
 
     @Test
-    void move() {
+    void moveTest() {
+
     }
 
     @Test
--- src/test/java/HorseTest.java
+++ src/test/java/HorseTest.java
@@ -1,13 +1,74 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.mockito.MockedStatic;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.mockStatic;
 
 class HorseTest {
     @ParameterizedTest
-//    @CsvSource({"null,1,1"})
-    void Horse(){
-        assertThrows(IllegalArgumentException.class,
-                ()-> Horse());
+    @NullAndEmptySource
+    @ValueSource(strings = {"  ", "\t", "\n"})
+    void horseWithNullOrEmptyName(String name) {
+        Throwable exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Horse(name, 1.0, 1.0)
+        );
+
+        if (name == null) {
+            assertEquals("Name cannot be null.", exception.getMessage());
+        } else if (name.isBlank()) {
+            assertEquals("Name cannot be blank.", exception.getMessage());
+        }
+
     }
 
+    @ParameterizedTest
+    @CsvSource({"-1,1", "1,-1"})
+    void horseWithNegativeSpeedOrDistance(double speed, double distance) {
+        Throwable exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> new Horse("name", speed, distance)
+        );
+
+        if (speed < 0)
+            assertEquals("Speed cannot be negative.", exception.getMessage());
+        else if (distance < 0)
+            assertEquals("Distance cannot be negative.", exception.getMessage());
+    }
+
+    @Test
+    void getHorseNameTest() {
+        String horseName = "Horse1";
+        Horse horse = new Horse(horseName, 1, 1);
+        assertEquals(horse.getName(), horseName);
+    }
+
+    @Test
+    void getHorseSpeedTest() {
+        double horseSpeed = 2.7;
+        Horse horse = new Horse("Horse1", horseSpeed, 1);
+        assertEquals(horse.getSpeed(), horseSpeed);
+    }
+
+    @Test
+    void getHorseDistanceTest() {
+        double horseDistance = 3;
+        Horse horse = new Horse("Horse1", 1, horseDistance);
+        assertEquals(horse.getDistance(), horseDistance, "Parameter distance not equals getDistance().");
+
+        Horse horse1 = new Horse("Horse1", 1);
+        assertEquals(horse1.getDistance(), 0, "getDistance() should return 0.");
+    }
+
+    @Test
+    void moveUseGetRandom() {
+        try (MockedStatic<Horse> mockedStatic = mockStatic(Horse.class)) {
+            new Horse("name", 1, 1).move();
+            mockedStatic.verify(() -> Horse.getRandomDouble(0.2, 0.9));
+        }
+    }
 }
